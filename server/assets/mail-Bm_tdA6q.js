23d7437be766b06f93d547fdcca39b5b21ad39d5
boipelomawasha-pers <boipelomawasha-pers@users.noreply.github.com>
2026-06-15 10:39:58 +0000

    deploy: 6c1328681c0e5f45bd94937309e1401d42fa3b8d

---
+++ server/assets/mail-Bm_tdA6q.js
@@ -0,0 +1,1 @@
+import{u as e}from"./card-DOgOMjFa.js";var t=e(`arrow-right`,[[`path`,{d:`M5 12h14`,key:`1ays0h`}],[`path`,{d:`m12 5 7 7-7 7`,key:`xquz4c`}]]),n=e(`mail`,[[`path`,{d:`m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7`,key:`132q7q`}],[`rect`,{x:`2`,y:`4`,width:`20`,height:`16`,rx:`2`,key:`izxlao`}]]);export{t as n,n as t};
